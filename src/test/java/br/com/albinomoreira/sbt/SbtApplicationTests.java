7bc64938cc4aef9c1eed1ce16be94c30bb119f34
Albino Moreira <albino.sistemas@gmail.com>
2021-01-13 11:07:46 -0300

    Commit Inicial criação do projeto

---
+++ src/test/java/br/com/albinomoreira/sbt/SbtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.albinomoreira.sbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
