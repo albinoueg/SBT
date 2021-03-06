a0e6517d7944aee78d630c81047a4c44d1d64458
Albino Moreira <albino.sistemas@gmail.com>
2021-03-06 17:35:52 -0300

    #Inclusão de beanvalidator

--- src/main/java/br/com/albinomoreira/sbt/controller/CargoController.java
+++ src/main/java/br/com/albinomoreira/sbt/controller/CargoController.java
@@ -7,9 +7,11 @@ import br.com.albinomoreira.sbt.service.DepartamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -33,12 +35,12 @@ public class CargoController {
     }
 
     @PostMapping("/salvar")
-    public String salvar(Cargo cargo, RedirectAttributes attr){
+    public String salvar(@Valid Cargo cargo, BindingResult result, RedirectAttributes attr){
 
-        if(cargo.getDepartamento() == null){
-            attr.addFlashAttribute("fail", "Selecione um departamento.");
-            return "redirect:/cargos/cadastrar";
+        if(result.hasErrors()){
+            return "/cargo/cadastro";
         }
+
         cargoService.salvar(cargo);
         attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
         return "redirect:/cargos/cadastrar";
@@ -51,7 +53,12 @@ public class CargoController {
     }
 
     @PostMapping("/editar")
-    public String editar(Cargo cargo, RedirectAttributes attr) {
+    public String editar(@Valid Cargo cargo, BindingResult result, RedirectAttributes attr) {
+
+        if(result.hasErrors()){
+            return "/cargo/cadastro";
+        }
+
         cargoService.editar(cargo);
         attr.addFlashAttribute("success", "Registro atualizado com sucesso.");
         return "redirect:/cargos/cadastrar";
