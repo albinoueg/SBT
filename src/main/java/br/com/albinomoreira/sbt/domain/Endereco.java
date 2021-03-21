9111481723d42cfed4c469152889a39cd7d249ee
Albino Moreira <albino.sistemas@gmail.com>
2021-03-21 13:35:49 -0300

    Utilização de ValidationMessages.properties para definir as mensagens de erro

--- src/main/java/br/com/albinomoreira/sbt/domain/Endereco.java
+++ src/main/java/br/com/albinomoreira/sbt/domain/Endereco.java
@@ -3,27 +3,42 @@ package br.com.albinomoreira.sbt.domain;
 import br.com.albinomoreira.sbt.enums.UF;
 
 import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "ENDERECOS")
 public class Endereco extends AbstractEntity<Long>{
 
+    @NotBlank
+    @Size(max = 255, min = 3)
     @Column(nullable = false)
     private String logradouro;
 
+    @NotBlank
+    @Size(max = 255, min = 3)
     @Column(nullable = false)
     private String bairro;
 
+    @NotBlank
+    @Size(max = 255, min = 3)
     @Column(nullable = false)
     private String cidade;
 
+    @NotNull(message = "{NotNull.endereco.uf}")
     @Column(nullable = false, length = 2)
     @Enumerated(EnumType.STRING)
     private UF uf;
 
+    @NotBlank
+    @Size(min = 9, max = 9, message = "{Size.endereco.cep}")
     @Column(nullable = false, length = 9)
     private String cep;
 
+    @NotNull(message = "{NotNull.endereco.numero}")
+    @Digits(integer = 5, fraction = 0)
     @Column(nullable = false, length = 5)
     private Integer numero;
 
